10cc7fd5fa4a2aa0d93e6ae2587555ee985fc681
racerandom <kevin.cheng.fei@gmail.com>
2020-10-06 15:22:01 +0900

    20201006

--- pipeline_bert_test.sh
+++ pipeline_bert_test.sh
@@ -8,15 +8,17 @@ BERT_URL="/home/feicheng/Tools/ClinicalBERT"
 CUDA_VISIBLE_DEVICES=$1 python clinical_pipeline_ner.py \
 --saved_model "checkpoints/tmp/pipeline/ner/${BERT}/" \
 --test_output "tmp/pipeline/test_${BERT}.ner" \
---batch_size 32 \
+--batch_size 64 \
 
 CUDA_VISIBLE_DEVICES=$1 python clinical_pipeline_mod.py \
 --saved_model "checkpoints/tmp/pipeline/mod/${BERT}/" \
+--test_file "tmp/pipeline/test_${BERT}.ner" \
 --test_output "tmp/pipeline/test_${BERT}.mod" \
---batch_size 32 \
+--batch_size 64 \
 
 CUDA_VISIBLE_DEVICES=$1 python clinical_pipeline_rel.py \
 --saved_model "checkpoints/tmp/pipeline/rel/${BERT}/" \
+--test_file "tmp/pipeline/test_${BERT}.mod" \
 --test_output "tmp/pipeline/test_${BERT}.rel" \
---batch_size 16 \
+--batch_size 32 \
 
