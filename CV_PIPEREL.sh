9180c06867ed311c1cfb97c3c38b3d83025e0489
racerandom <kevin.cheng.fei@gmail.com>
2020-11-23 22:07:52 +0900

    cv scripts update

---
+++ CV_PIPEREL.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+CORPUS=$2
+DOC_OR_SENT=$3
+SAVED_MODEL="checkpoints/piperel/${CORPUS}_${DOC_OR_SENT}_cv${cv_id}"
+TEST_FILE="data/2020Q2/${CORPUS}/${DOC_OR_SENT}_conll/cv${cv_id}_test.conll"
+TEST_OUTPUT="tmp/piperel_${CORPUS}_${DOC_OR_SENT}_cv${cv_id}_test.out"
+
+for cv_id in 0 1 2 3 4; do
+    CUDA_VISIBLE_DEVICES=$1 python clinical_pipeline_rel.py \
+    --train_file "data/2020Q2/${CORPUS}/${DOC_OR_SENT}_conll/cv${cv_id}_train.conll" \
+    --dev_file "data/2020Q2/${CORPUS}/${DOC_OR_SENT}_conll/cv${cv_id}_dev.conll" \
+    --dev_output "tmp/piperel_${CORPUS}_${DOC_OR_SENT}_cv${cv_id}_dev.out" \
+    --test_file $TEST_FILE \
+    --test_output $TEST_OUTPUT \
+    --saved_model $SAVED_MODEL \
+    --do_train \
+    --batch_size 1
+
+    CUDA_VISIBLE_DEVICES=$1 python clinical_pipeline_rel.py \
+    --saved_model $SAVED_MODEL \
+    --test_file $TEST_FILE \
+    --test_output $TEST_OUTPUT \
+    --batch_size 4
+done
+
