9463014a7f261a0d12124231dfe96b1d70881fdd
racerandom <kevin.cheng.fei@gmail.com>
2020-11-23 23:43:18 +0900

    add the annotated data to training

--- CV_JOINT.sh
+++ CV_JOINT.sh
@@ -2,25 +2,23 @@
 
 CORPUS=$2
 DOC_OR_SENT=$3
-SAVED_MODEL="checkpoints/joint/${CORPUS}_${DOC_OR_SENT}_cv${cv_id}"
-TEST_FILE="data/2020Q2/${CORPUS}/${DOC_OR_SENT}_conll/cv${cv_id}_test.conll"
-TEST_OUTPUT="tmp/joint_${CORPUS}_${DOC_OR_SENT}_cv${cv_id}_test.out"
+SAVED_MODEL="checkpoints/joint/${CORPUS}_${DOC_OR_SENT}"
+TEST_FILE="data/2020Q2/${CORPUS}/${DOC_OR_SENT}_conll"
+TEST_OUTPUT="tmp/joint_${CORPUS}_${DOC_OR_SENT}"
 
 for cv_id in 0 1 2 3 4; do
     CUDA_VISIBLE_DEVICES=$1 python clinical_joint.py \
     --train_file "data/2020Q2/${CORPUS}/${DOC_OR_SENT}_conll/cv${cv_id}_train.conll" \
     --dev_file "data/2020Q2/${CORPUS}/${DOC_OR_SENT}_conll/cv${cv_id}_dev.conll" \
     --dev_output "tmp/joint_${CORPUS}_${DOC_OR_SENT}_cv${cv_id}_dev.out" \
-    --test_file $TEST_FILE \
-    --test_output $TEST_OUTPUT \
-    --saved_model $SAVED_MODEL \
+    --saved_model "${SAVED_MODEL}_cv${cv_id}" \
     --do_train \
     --batch_size 1
 
     CUDA_VISIBLE_DEVICES=$1 python clinical_joint.py \
-    --saved_model $SAVED_MODEL \
-    --test_file $TEST_FILE \
-    --test_output $TEST_OUTPUT \
+    --saved_model "${SAVED_MODEL}_cv${cv_id}" \
+    --test_file "${TEST_FILE}/cv${cv_id}_test.conll" \
+    --test_output "${TEST_OUTPUT}_cv${cv_id}_test.out" \
     --batch_size 4
 done
 
